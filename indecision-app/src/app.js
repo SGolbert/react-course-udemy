9db61eca7e4c4330c928f2b3bcd27602c3b9582a
Sebastian Golbert <sebastian.golbert@gmail.com>
2020-02-28 23:30:34 +0100

    create a simple app with Babel and JSX

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,13 +1,7 @@
-"use strict";
-
 console.log("App.js is running!");
 
 // JSX - Javascript XML
-var template = React.createElement(
-  "p",
-  null,
-  " This is JSX from source "
-);
+var template = <p> This is JSX from source </p>;
 
 var appRoot = document.getElementById("app");
 
