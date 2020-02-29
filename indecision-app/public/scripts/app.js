9507b02e869648f6edb173fcfa8665654382734e
Sebastian Golbert <sebastian.golbert@gmail.com>
2020-02-29 14:07:18 +0100

    try JSX initial challenge

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -15,9 +15,44 @@ var template = React.createElement(
     "p",
     null,
     "Udemy course"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item 1"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item 2"
+    )
+  )
+);
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Sebastian Golbert"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: 35"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: M\xFCnster, Germany"
   )
 );
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+// ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -5,9 +5,22 @@ var template = (
   <div>
     <h1> Indecision App! </h1>
     <p>Udemy course</p>
+    <ol>
+      <li>Item 1</li>
+      <li>Item 2</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1>Sebastian Golbert</h1>
+    <p>Age: 35</p>
+    <p>Location: Münster, Germany</p>
   </div>
 );
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+// ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
