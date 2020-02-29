c09401dff11d209224d540a1189a6844518e5d93
Sebastian Golbert <sebastian.golbert@gmail.com>
2020-02-29 18:39:21 +0100

    add decision button

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,78 +3,90 @@
 console.log("App.js is running!");
 
 var appData = {
-    title: "Indecision? Away!",
-    subtitle: "resolving all your doubts",
-    options: ["one", "two"]
+  title: "Indecision? Away!",
+  subtitle: "resolving all your doubts",
+  options: ["one", "two"]
 };
 
 function onFormSubmit(e) {
-    e.preventDefault();
-    console.log("form submitted!");
-    var option = e.target.elements.option.value;
+  e.preventDefault();
+  console.log("form submitted!");
+  var option = e.target.elements.option.value;
 
-    if (option) {
-        appData.options.push(option);
-        e.target.elements.option.value = "";
-        renderTemplate();
-    }
+  if (option) {
+    appData.options.push(option);
+    e.target.elements.option.value = "";
+    renderTemplate();
+  }
 }
 
 function resetList() {
-    appData.options = [];
-    renderTemplate();
+  appData.options = [];
+  renderTemplate();
 }
 
 var appRoot = document.getElementById("app");
 
-function renderTemplate() {
-    var template = React.createElement(
-        "div",
-        null,
-        React.createElement(
-            "h1",
-            null,
-            appData.title
-        ),
-        appData.subtitle ? React.createElement(
-            "p",
-            null,
-            appData.subtitle
-        ) : "",
-        React.createElement(
-            "p",
-            null,
-            appData.options.length > 0 ? "Here are your options:" : "No options :("
-        ),
-        React.createElement(
-            "button",
-            { onClick: resetList },
-            "Reset list"
-        ),
-        React.createElement(
-            "ol",
-            null,
-            appData.options.map(function (opt) {
-                return React.createElement(
-                    "li",
-                    { key: opt },
-                    opt
-                );
-            })
-        ),
-        React.createElement(
-            "form",
-            { onSubmit: onFormSubmit },
-            React.createElement("input", { type: "text", name: "option" }),
-            React.createElement(
-                "button",
-                null,
-                "Add option"
-            )
-        )
-    );
+function onMakeDecision() {
+  var randomNum = Math.floor(Math.random() * appData.options.length);
+  var option = appData.options[randomNum];
+  console.log(randomNum);
+  alert(option);
+}
 
-    ReactDOM.render(template, appRoot);
+function renderTemplate() {
+  var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      appData.title
+    ),
+    appData.subtitle ? React.createElement(
+      "p",
+      null,
+      appData.subtitle
+    ) : "",
+    React.createElement(
+      "p",
+      null,
+      appData.options.length > 0 ? "Here are your options:" : "No options :("
+    ),
+    React.createElement(
+      "button",
+      { disabled: appData.options.length == 0, onClick: onMakeDecision },
+      "What should I do?"
+    ),
+    React.createElement(
+      "button",
+      { onClick: resetList },
+      "Reset list"
+    ),
+    React.createElement(
+      "ol",
+      null,
+      appData.options.map(function (opt) {
+        return React.createElement(
+          "li",
+          { key: opt },
+          opt
+        );
+      })
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: onFormSubmit },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        null,
+        "Add option"
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
 }
 
 renderTemplate();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,55 +1,65 @@
 console.log("App.js is running!");
 
 const appData = {
-    title: "Indecision? Away!",
-    subtitle: "resolving all your doubts",
-    options: ["one", "two"]
+  title: "Indecision? Away!",
+  subtitle: "resolving all your doubts",
+  options: ["one", "two"]
 };
 
 function onFormSubmit(e) {
-    e.preventDefault();
-    console.log("form submitted!");
-    const option = e.target.elements.option.value;
+  e.preventDefault();
+  console.log("form submitted!");
+  const option = e.target.elements.option.value;
 
-    if (option) {
-        appData.options.push(option);
-        e.target.elements.option.value = "";
-        renderTemplate();
-    }
+  if (option) {
+    appData.options.push(option);
+    e.target.elements.option.value = "";
+    renderTemplate();
+  }
 }
 
 function resetList() {
-    appData.options = [];
-    renderTemplate();
+  appData.options = [];
+  renderTemplate();
 }
 
 const appRoot = document.getElementById("app");
 
+function onMakeDecision() {
+  const randomNum = Math.floor(Math.random() * appData.options.length);
+  const option = appData.options[randomNum];
+  console.log(randomNum);
+  alert(option);
+}
+
 function renderTemplate() {
-    const template = (
-        <div>
-            <h1>{appData.title}</h1>
-            {appData.subtitle ? <p>{appData.subtitle}</p> : ""}
-            <p>
-                {appData.options.length > 0
-                    ? "Here are your options:"
-                    : "No options :("}
-            </p>
-            <button onClick={resetList}>Reset list</button>
-            <ol>
-                {appData.options.map(opt => (
-                    <li key={opt}>{opt}</li>
-                ))}
-            </ol>
+  const template = (
+    <div>
+      <h1>{appData.title}</h1>
+      {appData.subtitle ? <p>{appData.subtitle}</p> : ""}
+      <p>
+        {appData.options.length > 0
+          ? "Here are your options:"
+          : "No options :("}
+      </p>
+      <button disabled={appData.options.length == 0} onClick={onMakeDecision}>
+        What should I do?
+      </button>
+      <button onClick={resetList}>Reset list</button>
+      <ol>
+        {appData.options.map(opt => (
+          <li key={opt}>{opt}</li>
+        ))}
+      </ol>
 
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option"></input>
-                <button>Add option</button>
-            </form>
-        </div>
-    );
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"></input>
+        <button>Add option</button>
+      </form>
+    </div>
+  );
 
-    ReactDOM.render(template, appRoot);
+  ReactDOM.render(template, appRoot);
 }
 
 renderTemplate();
