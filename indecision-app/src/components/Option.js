3f78a90c62d313426ab62ab8b5b71616206bd67c
Sebastian Golbert <sebastian.golbert@gmail.com>
2020-03-02 18:26:08 +0100

    use shorthand return statements

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -1,14 +1,12 @@
 import React from "react";
 
-const Option = props => {
-  return (
-    <li>
-      {props.text}{" "}
-      <button onClick={e => props.handleDeleteSingleOption(props.text)}>
-        Remove
-      </button>
-    </li>
-  );
-};
+const Option = props => (
+  <li>
+    {props.text}{" "}
+    <button onClick={e => props.handleDeleteSingleOption(props.text)}>
+      Remove
+    </button>
+  </li>
+);
 
 export default Option;
