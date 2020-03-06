82f2b2f952496a7b31be5fbdb657707e5cc2c8e9
Sebastian Golbert <sebastian.golbert@gmail.com>
2020-03-06 22:34:30 +0100

    add Navlinks and move components to separate files

---
+++ expensify-app/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>
+      <p>Dashboard</p>
+    </NavLink>
+    <NavLink to="/create" activeClassName="is-active">
+      <p>Create</p>
+    </NavLink>
+    <NavLink to="/edit" activeClassName="is-active">
+      <p>Edit</p>
+    </NavLink>
+    <NavLink to="/help" activeClassName="is-active">
+      <p>Help</p>
+    </NavLink>
+  </header>
+);
+
+export default Header;
